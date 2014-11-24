e48e0883d21bb9c07e97551bfce29f6b7741102d
Graham Mueller <wgmueller@gmail.com>
2014-11-24 09:38:52 -0500

    remove print

--- R/arima_all.R
+++ R/arima_all.R
@@ -10,7 +10,6 @@
 
 arima_all <-function(x){
   fit<-auto.arima(x)
-  print(length(fit$model$phi)+length(fit$model$theta))
   bt<-Box.test(fit$residuals,lag=5,fitdf=length(fit$model$phi)+length(fit$model$theta))
   return(list("p.value"=bt$p.value,"resid"=as.vector(fit$resid),"phi"=as.vector(fit$model$phi),"theta"=as.vector(fit$model$theta),"D"=as.vector(fit$model$D)))
   
